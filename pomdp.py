a8ca42858e5f279c8c7a5c100626cd138fc37715
Chris Mansley <cmansley@cs.rutgers.edu>
2011-12-07 22:05:39 -0500

    Add lots of stuff

--- pomdp.py
+++ pomdp.py
@@ -3,8 +3,11 @@ import random
 
 import itertools
 
+from collections import defaultdict
+
 from cvxopt import matrix 
 from cvxopt import solvers
+from cvxopt import mul
 
 solvers.options['show_progress'] = False
 
@@ -37,19 +40,71 @@ def dotproduct(vec1, vec2):
 
 
 #
-# Model Specific Functions
+# Model Specific Class
 #
-def tau(state, action, nextstate, alpha):
-    pass
+class Model:
+    def __init__(self, S, A, O, R, T, M):
+        self.S = S
+        self.A = A
+        self.O = O
+        self.R = R
+        self.T = T
+        self.M = M
 
-# def tau(action, observation, alpha):
-#    "POMDPs"
-#    pass
+    def tau(self, action, observation, S):
+        """POMDPs"""
+        ts = []
+        for alpha in S:
+            t1 = mul(alpha, self.M[(observation,action)])
+            t2 = self.T[action] * t1
+            ts.append(t2)
+
+        print 'tau'
+        print ts[0].size
+        print 'end tau'
+
+        return ts
+
+    # def tau(state, action, nextstate, S):
+    #    """HM-MDPs"""
+    #    pass
 
 
 #
 # Incremental Pruning Functions
 #
+def vi(pomdp):
+    S = []
+    for a in pomdp.A:
+        S.append(R[a])
+
+    for x in range(2):
+        Saz = {}
+        Sa = {}
+        Su = []
+        for action in pomdp.A:
+            for obs in pomdp.O:
+                Saz[obs] = filter(pomdp.tau(action, obs, S))
+            Sa[action] = incprune(Saz)
+                
+            # badly create union
+            for v in Sa[action]:
+                Su.append(v)
+
+        print 'Su',Su[0].size
+        S = filter(Su)
+
+    return S
+
+
+def incprune(Saz):
+    vects = Saz.values()
+    W = filter(crosssum(vects[0],vects[1]))
+    for i in range(2, len(vects)):
+        W = filter(crosssum(W,vects[i]))
+
+    return W
+
 
 def crosssum(A,B):
     """
@@ -66,24 +121,24 @@ def filter(F):
     """
     wi = set()
     fi = set(range(len(F)))
-    for phi in F:
-        _, pos = argmax(phi, lambda x: x[i])
+    for i in range(4): # fix me!!!
+        _, pos = argmax(F, lambda x: x[i])
         wi.add(pos)
         fi.discard(pos)
 
     while fi:
-        W = [F[x] for x in wi]
+        W = [F[ii] for ii in wi]
         i = fi.pop()
         phi = F[i]
         x = dominate(phi, W)
         
         if x:
             fi.add(i)
-            _, pos = argmax([F[x] for x in fi], lambda y: dot(x,y))
+            _, pos = argmax([F[ii] for ii in fi], lambda y: dot(x,y))
             wi.add(pos)
             fi.discard(pos)
             
-    return [F[x] for x in wi]
+    return [F[ii] for ii in wi]
         
         
 
@@ -127,7 +182,7 @@ def dominate(alpha, setA):
     
     # x (alpha_p - alpha) + delta <= 0
     for vector in setA:
-        da = vector - alpha
+        da = vector.T - alpha.T
         t4 = matrix([1.0])
         t5 = matrix([da, t4])
         A = matrix([A,t5.T])
@@ -165,11 +220,33 @@ def dominate(alpha, setA):
 # Test Suite
 #
 if __name__ == "__main__":
-    alpha = matrix([0.2,0.2])
-    
-    t1 = matrix([0.1,0.4])
 
-    A = []
-    A.append(t1)
+    #
+    # Littman 1D
+    #
 
-    print dominate(alpha, A)
+    S = ['left', 'middle', 'right', 'goal']
+    A = ['w0', 'e0']
+    O = ['nothing', 'goal']
+
+    R = {}
+    R['w0'] = matrix([0, 0, 0, 1])
+    R['e0'] = matrix([0, 0, 0, 1])
+
+    # transitions P(s'|s,a) or T[(s',a)] = [s1,s2, ...]
+    T = {}
+    T['w0'] = matrix([[1.0, 0.0, 0.0, 0.0],[1.0, 0.0, 0.0, 0.0],[0.0, 0.0, 0.0, 1.0],[0.333333, 0.333333, 0.333333, 0.0]]).T
+    T['e0'] = matrix([[0.0, 1.0, 0.0, 0.0],[0.0, 0.0, 0.0, 1.0],[0.0, 0.0, 1.0, 0.0],[0.333333, 0.333333, 0.333333, 0.0]]).T
+
+    # observation emmissions P(z|s,a) or M[(z,a)] = [s1,s2 ..]
+    M = {}
+    M[('nothing','w0')] = matrix([1.0, 1.0, 1.0, 0.0])
+    M[('goal','w0')] = matrix([0.0, 0.0, 0.0, 1.0])
+
+    M[('nothing','e0')] = matrix([1.0, 1.0, 1.0, 0.0])
+    M[('goal','e0')] = matrix([0.0, 0.0, 0.0, 1.0])
+
+    pomdp = Model(S,A,O,R,T,M)
+
+    s = vi(pomdp)
+    print s[0],s[1]
